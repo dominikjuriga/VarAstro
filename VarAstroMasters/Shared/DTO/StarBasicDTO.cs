d6e4593a346c38fdcc0d3ab1e2999775666b30ae
dominikjuriga <jurigadominik@gmail.com>
2022-05-11 12:20:23 +0200

    dalsie mudblazor komponenty

--- VarAstroMasters/Shared/DTO/StarBasicDTO.cs
+++ VarAstroMasters/Shared/DTO/StarBasicDTO.cs
@@ -8,5 +8,7 @@ public class StarBasicDTO
     public int Id { get; set; }
 
     public string Name { get; set; }
+    public double RA { get; set; }
+    public double DEC { get; set; }
     public List<StarCatalog> StarCatalogs { get; set; }
 }
