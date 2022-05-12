325ac5d12d88cd79400f7f6a0bae25ae91f674c9
dominikjuriga <jurigadominik@gmail.com>
2022-05-12 14:11:16 +0200

    comments for star coords model

--- VarAstroMasters/Shared/DTO/StarCoordDTO.cs
+++ VarAstroMasters/Shared/DTO/StarCoordDTO.cs
@@ -5,6 +5,7 @@ namespace VarAstroMasters.Shared.DTO;
 
 public class StarCoordDTO
 {
+    // RA is divided into [H]ours, [M]inutes and [S]econds
     [Required(ErrorMessage = Keywords.FormFieldRequired)]
     [Range(0, 23, ErrorMessage = Keywords.FormValueRange)]
     public int? RaH { get; set; }
@@ -13,10 +14,10 @@ public class StarCoordDTO
     [Range(0, 59, ErrorMessage = Keywords.FormValueRange)]
     public int? RaM { get; set; }
 
-
     [Required(ErrorMessage = Keywords.FormFieldRequired)]
     [Range(0, 59, ErrorMessage = Keywords.FormValueRange)]
     public int? RaS { get; set; }
+    // DEC is divided into [D]egress, Arc[M]inutes and Arc[S]econds
 
     [Required(ErrorMessage = Keywords.FormFieldRequired)]
     [Range(-90, 90, ErrorMessage = Keywords.FormValueRange)]
