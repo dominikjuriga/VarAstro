97f4b0af6d1ae7b0733ee9424fb3d14a826c3475
dominikjuriga <jurigadominik@gmail.com>
2022-05-12 14:54:25 +0200

    zariadenia a hvezdarne mudblazor

--- VarAstroMasters/Shared/Models/Observatory.cs
+++ VarAstroMasters/Shared/Models/Observatory.cs
@@ -8,14 +8,17 @@ public class Observatory
 {
     public int Id { get; set; }
 
+    [Required(ErrorMessage = Keywords.FormFieldRequired)]
     [Range(-180, 180, ErrorMessage = Keywords.FormValueRange)]
     public double Longitude { get; set; } = 16.606836d;
 
-
+    [Required(ErrorMessage = Keywords.FormFieldRequired)]
     [Range(-90, 90, ErrorMessage = Keywords.FormValueRange)]
     public double Latitude { get; set; } = 49.195061d;
 
+    [Required(ErrorMessage = Keywords.FormFieldRequired)]
     public string Address { get; set; }
+
     public string UserId { get; set; } = string.Empty;
     [NotMapped] public bool New { get; set; } = true;
 }
