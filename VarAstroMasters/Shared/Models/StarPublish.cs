df2f9fc055de6c249ebd67bcd5d1c311ea41b145
dominikjuriga <105605732+dominikjuriga@users.noreply.github.com>
2022-05-16 19:30:44 +0200

    fix uprava publikacie dataannotationvalidator

--- VarAstroMasters/Shared/Models/StarPublish.cs
+++ VarAstroMasters/Shared/Models/StarPublish.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using VarAstroMasters.Shared.Static;
 
 namespace VarAstroMasters.Shared.Models;
 
@@ -7,7 +9,13 @@ public class StarPublish
     public int Id { get; set; }
     public int StarId { get; set; } = -1;
     [NotMapped] public string? StarName { get; set; }
+
+    [Required(ErrorMessage = Keywords.FormFieldRequired)]
     public int Year { get; set; }
+
+    [Required(ErrorMessage = Keywords.FormFieldRequired)]
     public string Discoverer { get; set; }
+
+    [Required(ErrorMessage = Keywords.FormFieldRequired)]
     public string PublicationLink { get; set; }
 }
